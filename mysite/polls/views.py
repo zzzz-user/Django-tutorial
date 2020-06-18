f6297ea29d18d6d59299f1ccc37840b5e465db1b
sa <ta_qwerty@outlook.com>
2020-06-18 15:09:00 +0900

    update 404 シンプルバージョン

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,4 @@
-from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Question
 
@@ -13,10 +12,7 @@ def index(request):
     return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
